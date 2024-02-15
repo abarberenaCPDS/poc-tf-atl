c6e612229085a1a52d0d5b54edae0c693f25dbf3
abe <abe@me.com>
2024-02-15 13:43:54 -0800

    dummy test

--- main.tf
+++ main.tf
@@ -1,24 +1,27 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
-    }
-  }
+resource "null_resource" "example" {
+  
 }
+# terraform {
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "~> 3.0.2"
+#     }
+#   }
+# }
 
-provider "docker" {}
+# provider "docker" {}
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
+# resource "docker_image" "nginx" {
+#   name         = "nginx:latest"
+#   keep_locally = false
+# }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = var.container_name
-  ports {
-    internal = 80
-    external = 8080
-  }
-}
+# resource "docker_container" "nginx" {
+#   image = docker_image.nginx.image_id
+#   name  = var.container_name
+#   ports {
+#     internal = 80
+#     external = 8080
+#   }
+# }
