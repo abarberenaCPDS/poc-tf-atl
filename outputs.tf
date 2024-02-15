71a32863bd5cd263c324ac2e8669291a5dde76b8
abe <abe@me.com>
2024-02-15 13:45:37 -0800

    test

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "container_id" {
-  description = "The ID of the container"
-  value       = docker_container.nginx.id
-}
+# output "container_id" {
+#   description = "The ID of the container"
+#   value       = docker_container.nginx.id
+# }
 
-output "image_id" {
-  description = "The ID of the image"
-  value = docker_image.nginx.id
-}
+# output "image_id" {
+#   description = "The ID of the image"
+#   value = docker_image.nginx.id
+# }
